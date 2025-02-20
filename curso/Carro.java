70ee6a08cb36977e178f9465c62cab95e8299e8e
João Victor <jvs.devsilva@gmail.com>
2025-02-19 23:29:30 -0300

    Add Carro interface and implement Fusca class
    Introduce the `Carro` interface with four methods: `acelerar`, `frear`, `ligarTurbo`, and `desligarTurbo`. Create the `Fusca` class, which implements the `Carro` interface with empty method definitions. This sets up a structure for future implementation of car behaviors.

---
+++ curso/Carro.java
@@ -0,0 +1,9 @@
+package curso;
+
+public interface Carro {
+
+    void acelerar();
+    void frear();
+    void ligarTurbo();
+    void desligarTurbo();
+}
---
+++ curso/Fusca.java
@@ -0,0 +1,26 @@
+package curso;
+
+public class Fusca  implements Carro {
+
+
+    @Override
+    public void acelerar() {
+
+    }
+
+    @Override
+    public void frear() {
+
+    }
+
+    @Override
+    public void ligarTurbo() {
+
+    }
+
+    @Override
+    public void desligarTurbo() {
+     Carro meucarro = new Fusca();
+    }
+}
+
